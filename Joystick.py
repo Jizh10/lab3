3985cea3daef43bc145a0692fbfa3cba25f4d2af
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-10-06 19:34:06 +0000

    finished main

--- Joystick.py
+++ Joystick.py
@@ -2,15 +2,17 @@ import PCF8591
 
 class Joystick():
 
-  def __init__(self, address1, address2):
-    self.xVal = PCF8591(address1)
-    self.yVal = PCF8591(address2)
+  def __init__(self, address, chn1, chn2):
+    self.xVal = PCF8591(address)
+    self.yVal = PCF8591(address)
+    self.xChn = chn1
+    self.yChn = chn2
 
-  def getX(self, chn):
-    return self.xVal.read(chn)
+  def getX(self):
+    return self.xVal.read(self.xChn)
 
-  def getY(self, chn):
-    return self.yVal.read(chn)
+  def getY(self):
+    return self.yVal.read(self.yChn)
   
   
     
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#import PCF8591
+import Joystick
+import time
+
+xChn = 0
+yChn = 1
+address = 0x48
+
+joystick = Joystick(address, xChn, yChn)
+
+while True:
+  print("%4s, %4s" % (joystick.getX(), joystick.getY()))
+  time.sleep(0.1)
